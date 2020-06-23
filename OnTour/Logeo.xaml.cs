f6404dd80aa3baa2bb58d98c505f6816012081f5
Sebastian Riveros <riveross92@gmail.com>
2020-06-22 21:29:07 -0400

    su buen logeo

---
+++ OnTour/Logeo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnTour
+{
+    /// <summary>
+    /// Lógica de interacción para Logeo.xaml
+    /// </summary>
+    public partial class Logeo : Window
+    {
+        public Logeo()
+        {
+            InitializeComponent();
+        }
+    }
+}
